3790f733f28a9d05722731792e6908167bfb5d9a
Lautaro Castro <lautaro.castro.est@gmail.com>
2025-02-01 10:45:43 -0300

    Se corrige identanción

--- utils.c
+++ utils.c
@@ -2,11 +2,12 @@
 // Created by lgc on 21/01/25.
 //
 #include "utils.h"
+#include <stdio.h>
 
 int max(int a, int b) {
-    return a > b ? a : b;
+  return a > b ? a : b;
 }
 
 int min(int a, int b) {
-    return a < b ? a : b;
+  return a < b ? a : b;
 }
